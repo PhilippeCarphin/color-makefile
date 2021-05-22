a794a6b9b027af8d12223dc7ba703fc4de14614a
Philippe Carphin <philippe.carphin2@canada.ca>
2021-05-22 19:30:07 -0400

    update

--- colors.mk
+++ colors.mk
@@ -24,35 +24,35 @@ endif
 
 define make_echo_color_bold
 	@case $(1) in \
-		red)     $(ECHO) "\033[1;31m$(2)\033[0m" ;; \
-		green)   $(ECHO) "\033[1;32m$(2)\033[0m" ;; \
-		yellow)  $(ECHO) "\033[1;33m$(2)\033[0m" ;; \
-		blue)    $(ECHO) "\033[1;34m$(2)\033[0m" ;; \
-		magenta) $(ECHO) "\033[1;35m$(2)\033[0m" ;; \
-		cyan)    $(ECHO) "\033[1;36m$(2)\033[0m" ;; \
-		white)   $(ECHO) "\033[1;37m$(2)\033[0m" ;; \
-		*)       $(ECHO) "\033[1;4m$(2)\033[0m"  ;; \
+		red)     $(ECHO) $$'\033[1;31m'"$(2)"$$'\033[0m' ;; \
+		green)   $(ECHO) $$'\033[1;32m'"$(2)"$$'\033[0m' ;; \
+		yellow)  $(ECHO) $$'\033[1;33m'"$(2)"$$'\033[0m' ;; \
+		blue)    $(ECHO) $$'\033[1;34m'"$(2)"$$'\033[0m' ;; \
+		magenta) $(ECHO) $$'\033[1;35m'"$(2)"$$'\033[0m' ;; \
+		cyan)    $(ECHO) $$'\033[1;36m'"$(2)"$$'\033[0m' ;; \
+		white)   $(ECHO) $$'\033[1;37m'"$(2)"$$'\033[0m' ;; \
+		*)       $(ECHO) $$'\033[1;4m'"$(2)"$$'\033[0m' ;; \
 	esac
 endef
 
 define make_echo_color
 	@case $(1) in \
-		red)     $(ECHO) "\033[31m$(2)\033[0m" ;; \
-		green)   $(ECHO) "\033[32m$(2)\033[0m" ;; \
-		yellow)  $(ECHO) "\033[33m$(2)\033[0m" ;; \
-		blue)    $(ECHO) "\033[34m$(2)\033[0m" ;; \
-		magenta) $(ECHO) "\033[35m$(2)\033[0m" ;; \
-		cyan)    $(ECHO) "\033[36m$(2)\033[0m" ;; \
-		white)   $(ECHO) "\033[37m$(2)\033[0m" ;; \
-		*)       $(ECHO) "\033[4m$(2)\033[0m"  ;; \
+		red)     $(ECHO) $$'\033[31m'"$(2)"$$'\033[0m' ;; \
+		green)   $(ECHO) $$'\033[32m'"$(2)"$$'\033[0m' ;; \
+		yellow)  $(ECHO) $$'\033[33m'"$(2)"$$'\033[0m' ;; \
+		blue)    $(ECHO) $$'\033[34m'"$(2)"$$'\033[0m' ;; \
+		magenta) $(ECHO) $$'\033[35m'"$(2)"$$'\033[0m' ;; \
+		cyan)    $(ECHO) $$'\033[36m'"$(2)"$$'\033[0m' ;; \
+		white)   $(ECHO) $$'\033[37m'"$(2)"$$'\033[0m' ;; \
+		*)       $(ECHO) $$'\033[4m'"$(2)"$$'\033[0m' ;; \
 	esac
 endef
 
 define big_success
-	@$(ECHO) "$@ : \033[1;32m✓\033[0m"
+	@$(ECHO) "$@ : "$$'\033[1;32m✓\033[0m'
 endef
 define success
-	@$(ECHO) "$@ : \033[32m✓\033[0m"
+	@$(ECHO) "$@ : "$$'\033[32m✓\033[0m'
 endef
 define make_echo_build_c_object
 	$(call make_echo_color,green,"Building C object $@")
