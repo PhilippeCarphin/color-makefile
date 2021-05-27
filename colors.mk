885de4adfce0a23dafdd24160bef835f6e76b31b
Philippe Carphin <philippe.carphin2@canada.ca>
2021-05-27 08:45:45 -0400

    Put actual \033 bytes in the text, unbeatable!

--- colors.mk
+++ colors.mk
@@ -16,50 +16,50 @@ endif
 
 define make_echo_color_bold
 	@case $(1) in \
-		red)     $(ECHO) $$'\033[1;31m'"$(2)"$$'\033[0m' ;; \
-		green)   $(ECHO) $$'\033[1;32m'"$(2)"$$'\033[0m' ;; \
-		yellow)  $(ECHO) $$'\033[1;33m'"$(2)"$$'\033[0m' ;; \
-		blue)    $(ECHO) $$'\033[1;34m'"$(2)"$$'\033[0m' ;; \
-		magenta) $(ECHO) $$'\033[1;35m'"$(2)"$$'\033[0m' ;; \
-		cyan)    $(ECHO) $$'\033[1;36m'"$(2)"$$'\033[0m' ;; \
-		white)   $(ECHO) $$'\033[1;37m'"$(2)"$$'\033[0m' ;; \
-		*)       $(ECHO) $$'\033[1;4m'"$(2)"$$'\033[0m' ;; \
+		red)     $(ECHO) '[1;31m$(2)[0m' ;; \
+		green)   $(ECHO) '[1;32m$(2)[0m' ;; \
+		yellow)  $(ECHO) '[1;33m$(2)[0m' ;; \
+		blue)    $(ECHO) '[1;34m$(2)[0m' ;; \
+		magenta) $(ECHO) '[1;35m$(2)[0m' ;; \
+		cyan)    $(ECHO) '[1;36m$(2)[0m' ;; \
+		white)   $(ECHO) '[1;37m$(2)[0m' ;; \
+		*)       $(ECHO) '[1;4m$(2)[0m' ;; \
 	esac
 endef
 
 define make_echo_color
 	@case $(1) in \
-		red)     $(ECHO) $$'\033[31m'"$(2)"$$'\033[0m' ;; \
-		green)   $(ECHO) $$'\033[32m'"$(2)"$$'\033[0m' ;; \
-		yellow)  $(ECHO) $$'\033[33m'"$(2)"$$'\033[0m' ;; \
-		blue)    $(ECHO) $$'\033[34m'"$(2)"$$'\033[0m' ;; \
-		magenta) $(ECHO) $$'\033[35m'"$(2)"$$'\033[0m' ;; \
-		cyan)    $(ECHO) $$'\033[36m'"$(2)"$$'\033[0m' ;; \
-		white)   $(ECHO) $$'\033[37m'"$(2)"$$'\033[0m' ;; \
-		*)       $(ECHO) $$'\033[4m'"$(2)"$$'\033[0m' ;; \
+		red)     $(ECHO) '[31m$(2)[0m' ;; \
+		green)   $(ECHO) '[32m$(2)[0m' ;; \
+		yellow)  $(ECHO) '[33m$(2)[0m' ;; \
+		blue)    $(ECHO) '[34m$(2)[0m' ;; \
+		magenta) $(ECHO) '[35m$(2)[0m' ;; \
+		cyan)    $(ECHO) '[36m$(2)[0m' ;; \
+		white)   $(ECHO) '[37m$(2)[0m' ;; \
+		*)       $(ECHO) '[4m$(2)[0m' ;; \
 	esac
 endef
 
 define big_success
-	@$(ECHO) "$@ : "$$'\033[1;32m✓\033[0m'
+	@$(ECHO) '$@ : [1;32m✓[0m'
 endef
 define success
-	@$(ECHO) "$@ : "$$'\033[32m✓\033[0m'
+	@$(ECHO) '$@ : [32m✓[0m'
 endef
 define make_echo_build_c_object
-	$(call make_echo_color,green,"Building C object $@")
+	$(call make_echo_color,green,Building C object $@)
 endef
 define make_echo_link_fortran_executable
-	$(call make_echo_color_bold,green,"Linking Fortran executable $@")
+	$(call make_echo_color_bold,green,Linking Fortran executable $@)
 endef
 define make_echo_build_fortran_object
-	$(call make_echo_color,green,"Building Fortran object $@")
+	$(call make_echo_color,green,Building Fortran object $@)
 endef
 define make_echo_link_c_executable
-	$(call make_echo_color_bold,green,"Linking C executable $@")
+	$(call make_echo_color_bold,green,Linking C executable $@)
 endef
 define make_echo_generate_file
-	$(call make_echo_color_bold,blue,"Generating $@")
+	$(call make_echo_color_bold,blue,Generating $@)
 endef
 define make_echo_run_test
 	$(call make_echo_color_bold,cyan,$@ : $(1))
